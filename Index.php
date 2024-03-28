57dd358336e39d06c422dc02f93818dada88fb31
Adama Dabo <dabodabo9064@gmail.com>
2024-03-28 12:48:31 +0000

    On a fait l'heritage de voiture de sa classe parent vehicule

--- Index.php
+++ Index.php
@@ -1,21 +1,41 @@
 <?php
-class Voiture {
+//Création d'une classe parent vehicule 
+class Vehicule {
     public $marque;
     public  $modele;
     public  $couleur;
     public $kilometrage;
     public $annee;
-
-    public function Afficher () {
-        echo "La voiture est une ".  $this->marque ." ".$this->modele." de couleur ". $this->couleur. "<br>";
-        echo "Elle a été fabriquée en ".$this->annee." et elle a fait ". $this->kilometrage." km.<br>";
+    //Constructeur de la class vehicule qui va prendre en parametre les attributs du vehicule
+    function __construct($marque, $modele, $couleur, $kilometrage, $annee) {
+        $this->marque = $marque;
+        $this->modele = $modele;
+        $this->couleur = $couleur;
+        $this->kilometrage = $kilometrage;
+        $this->annee = $annee;
+    }
+    function demarrer(){
+        echo "Le véhicule démarre.<br>";
     }
 }
-$voiture1 = new Voiture();
-$voiture1 -> marque= "Renault";
-$voiture1 -> modele="Clio";
-$voiture1 -> couleur="Bleu";
-$voiture1 -> annee=2005;
-$voiture1 -> kilometrage=98765;
+
+//Creation d'un sous-classe "voiture" qui hérite des attributs et méthodes de la  class "vehicule". Elle ajoute un nouvel attribut "?
+
+class Voiture extends Vehicule {
+    public $nb_place;
+    // La méthode __call est une méthode magique PHP qui permet de gérer les appels à des méthodes qui ne sont pas définies dans notre classe
+     
+public function  __construct($marque, $modele, $couleur, $kilometrage, $annee, $nb_place) {
+    parent::__construct($marque,$modele,$couleur,$kilometrage,$annee);
+    $this->nb_place= $nb_place;
+} 
+     function Afficher () {
+        echo "La voiture est une ".  $this->marque ." ".$this->modele." de couleur ". $this->couleur. "<br>";
+        echo "Elle a été fabriquée en ".$this->annee." et elle a fait ". $this->kilometrage." km. $this->nb_place<br>";
+    }
+
+}
+$voiture1 = new Voiture("Ford", "Mustang", "Rouge Vif", 2023, 100, 5);
+
 //On appelle la méthode d'objet pour afficher les informations de la première voiture
 $voiture1->Afficher();
